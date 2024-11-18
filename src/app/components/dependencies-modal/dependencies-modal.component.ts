87b1a9c300b19d260239a5d139c8b12ec1aabb27
Art-byte <arturo_pdrzbn@hotmail.com>
2024-11-17 19:10:41 -0600

    feat: Se carga la informacion de las dependencias dentro del modal junto con sus respectivos radio buttons

--- src/app/components/dependencies-modal/dependencies-modal.component.ts
+++ src/app/components/dependencies-modal/dependencies-modal.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {DependenciesValues} from "../../shared/models/dependencies-values";
 
 @Component({
   selector: 'app-dependencies-modal',
@@ -7,9 +8,15 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   templateUrl: './dependencies-modal.component.html',
   styleUrl: './dependencies-modal.component.css'
 })
-export class DependenciesModalComponent {
+export class DependenciesModalComponent implements OnInit{
+
   @Input() isVisible: boolean = false;
+  @Input() dependencies: DependenciesValues[] = [];
   @Output() closeModal = new EventEmitter<void>();
+
+  ngOnInit(): void {
+    console.log("Dependencies => " + this.dependencies);
+  }
 
   close() {
     this.closeModal.emit();
--- src/app/shared/models/dependencies-values.ts
+++ src/app/shared/models/dependencies-values.ts
@@ -1,6 +1,7 @@
 import {DependenciesContent} from "./dependencies-content";
 
 export class DependenciesValues {
+  id: string;
   sectionName: string;
   dependenciesContents: DependenciesContent[];
 }
